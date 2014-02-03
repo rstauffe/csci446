65174a4d44be2af5c0a994a9c83cdbdc683d562d
Ryan Stauffer <rstauffe@mymail.mines.edu>
2014-02-03 10:32:49 -0700

    added cats scaffold, basic display changes, added seeds.db

--- project04/pets/db/seeds.rb
+++ project04/pets/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cat.delete_all
+Cat.create(name: 'Charles', breed: 'Tabby', image_url: 'tabby-cat.jpg', 
+	description: 
+	%{<p>Charles is a 5-year-old tabby looking for a good home. He has a tendancy
+		to eat shoelaces, but he's also a lapcat for those who like a lover.</p>})
+		
+Cat.create(name: 'Naomi', breed: 'Siamese', image_url: 'siamese.jpg', 
+	description:
+	%{<p>Naomi is a 2-year-old Siamese cat. Originally a stray, she has adapted well
+		to domestic life, and she's ready to find a good home.</p>})
